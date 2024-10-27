c337b5264b6fee08d7cdb335f1d5294af74e7de7
jlb84 <jlb84@kent.ac.uk>
2024-10-27 21:37:35 +0000

    Update AllTogether.py

--- AllTogether.py
+++ AllTogether.py
@@ -3,8 +3,8 @@ random_numbers = [random.randint(1, 100) for _ in range(10)]
 
 print("Original list:", random_numbers)
 
-for number in random_numbers[:]: 
-    while number % 2 == 0 and number in random_numbers:
-        random_numbers.remove(number)
+for i in range(len(random_numbers) - 1, -1, -1):
+    if random_numbers[i] % 2 == 0:
+        random_numbers.pop(i)
 
 print("Remaining odd numbers:", random_numbers)
